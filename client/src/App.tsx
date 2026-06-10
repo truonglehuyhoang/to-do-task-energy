9cf054a4fb8094f3830336215d050d2cb6476cad
Truong Le Huy Hoang <hoanghuypeter1182004@gmail.com>
2026-06-10 15:41:59 +0700

    feat(init): refactor client application structure and integrate new dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <div>Home page</div>;
+};
+
+export default Index;
